474636759924fb52eed1c2ab2e8302b8bc139bd3
Divyam <divyamsaxena123@gmail.com>
2020-08-09 17:48:10 +0530

    Created Tooltip using react-tooltip

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactTooltip from 'react-tooltip';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      place: "top",
+      condition: false
+    };
+  }
+  changePlace(place) {
+    this.setState({
+      place: place
+    });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onClick() {
+    this.setState({
+      condition: true
+    });
+  }
+  render() {
+    const { place } = this.state;
+    return (
+      <div>
+        <section className="tooltip">
+          <div className="hover">
+            <a
+              data-for="main"
+              data-tip="Hello Tooltip"
+              data-iscapture="true"
+            >Hover 
+            </a>
+          </div>
+          <div className="direction">
+            <div className="buttons">
+              <div className="item">
+                <a
+                  className={place === "top" ? "active" : ""}
+                  onClick={this.changePlace.bind(this, "top")}
+                >
+                  Top
+                </a>
+                <a
+                  className={place === "right" ? "active" : ""}
+                  onClick={this.changePlace.bind(this, "right")}
+                >
+                  Right
+                </a>
+                <a
+                  className={place === "bottom" ? "active" : ""}
+                  onClick={this.changePlace.bind(this, "bottom")}
+                >
+                  Bottom
+                </a>
+                <a
+                  className={place === "left" ? "active" : ""}
+                  onClick={this.changePlace.bind(this, "left")}
+                >
+                  Left
+                </a>
+              </div>
+            </div>
+          </div>
+          <ReactTooltip
+            id="main"
+            place={place}
+            multiline={true}
+          />
+        </section>
+      </div>
+    );
+  }
 }
-
 export default App;
